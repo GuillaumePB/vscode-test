6b82d7fdbda03da5a8176e3984ef6a71e2f377f0
Guillaume PB <guillaume.proteau.b@gmail.com>
2021-07-12 11:09:24 -0400

    Remove some code

--- script.py
+++ script.py
@@ -1,18 +1,4 @@
-import math
-import os
-import sys
-import tkinter as tk
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who):
-    test = "test"
-    greeting = "hey"
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
